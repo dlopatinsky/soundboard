5b95024b0452bb2495adee446306379afd583e8d
dlopatinsky <lopatinskij.db@spbstu.ru>
2022-12-03 07:15:13 +0300

    fix quotes

--- src/tts_module/tts.py
+++ src/tts_module/tts.py
@@ -8,20 +8,20 @@ from src.config_module.config import Config
 class TTS:
     def __init__(self, config: Config):
         self._config = config
-        self._device = torch.device(self._config.get_from_tts_config("TTS Settings", "device_name"))
+        self._device = torch.device(self._config.get_from_tts_config('TTS Settings', 'device_name'))
 
     def set_device(self, device_name: str):
         self._device = torch.device(device_name)
 
     def run(self, text: str, profile: str, path: Path):
-        model, _ = torch.hub.load(repo_or_dir=self._config.get_from_tts_config(profile, "repository"),
-                                  model=self._config.get_from_tts_config(profile, "model"),
-                                  language=self._config.get_from_tts_config(profile, "language"),
-                                  speaker=self._config.get_from_tts_config(profile, "model_id"))
+        model, _ = torch.hub.load(repo_or_dir=self._config.get_from_tts_config(profile, 'repository'),
+                                  model=self._config.get_from_tts_config(profile, 'model'),
+                                  language=self._config.get_from_tts_config(profile, 'language'),
+                                  speaker=self._config.get_from_tts_config(profile, 'model_id'))
         model.to(self._device)
         if os.path.exists(text):
             os.remove(text)
         model.save_wav(text=text,
-                       speaker=self._config.get_from_tts_config(profile, "speaker"),
-                       sample_rate=int(self._config.get_from_tts_config(profile, "sample_rate")),
+                       speaker=self._config.get_from_tts_config(profile, 'speaker'),
+                       sample_rate=int(self._config.get_from_tts_config(profile, 'sample_rate')),
                        audio_path=str(path))
