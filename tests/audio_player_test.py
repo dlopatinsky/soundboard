9717c435e7330bc59e515bf6709dea457b8cce7c
dlopatinsky <lopatinskij.db@spbstu.ru>
2022-12-04 23:35:03 +0300

    fix file paths in tests

--- tests/audio_player_test.py
+++ tests/audio_player_test.py
@@ -6,7 +6,7 @@ import pytest
 from src.audio_module.audio_player import AudioPlayer
 
 
-@pytest.mark.parametrize('path, expected_exception', [('resources/test.txt', openal.OalError),
+@pytest.mark.parametrize('path, expected_exception', [('tests/resources/test.txt', openal.OalError),
                                   ('resources/test.nonexistent', openal.OalError)])
 def test_audio_player_play_sound_exceptions(path, expected_exception):
     with pytest.raises(expected_exception):
@@ -14,19 +14,19 @@ def test_audio_player_play_sound_exceptions(path, expected_exception):
         audio_player.play_sound(Path(path))
 
 
-@pytest.mark.parametrize('path', [('resources/test.wav')])
+@pytest.mark.parametrize('path', [('tests/resources/test.wav')])
 def test_audio_player_play_sound_normal(path):
     audio_player = AudioPlayer()
     audio_player.play_sound(Path(path))
 
 
 @pytest.mark.parametrize('paths', [(),
-                                   (('resources/test.wav', 'resources/test1.wav', 'resources/test2.wav')),
-                                   (('resources/test.wav', 'resources/test.wav', 'resources/test.wav'))])
+                                   (('tests/resources/test.wav', 'tests/resources/test1.wav', 'tests/resources/test2.wav')),
+                                   (('tests/resources/test.wav', 'tests/resources/test.wav', 'tests/resources/test.wav'))])
 def test_audio_player_get_audio_list(paths):
     expected_list = []
     audio_player = AudioPlayer()
     for path in paths:
         audio_player.play_sound(Path(path))
         expected_list.append(Path(path))
-    assert audio_player.get_audio_list() == expected_list
+    assert result == expected_list
